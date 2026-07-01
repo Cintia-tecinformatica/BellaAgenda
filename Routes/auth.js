2f71102fdb78bd51449a5242de1d12dad172da1d
Cintia Caetano Carvalho <caetanocintia4@gmail.com>
2026-07-01 19:53:17 -0300

    fix: padronizando pasta Controllers com C maiusculo no git

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const AuthController = require('../Controllers/auth');
+const authController = require('../Controllers/auth');
 
 // Telas (GET)
 router.get('/login', authController.renderLogin);

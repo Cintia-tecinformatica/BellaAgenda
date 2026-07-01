53681cd08a37416bc185d1de000c4196a22830b9
Cintia Caetano Carvalho <caetanocintia4@gmail.com>
2026-07-01 19:41:53 -0300

    fix: corrigindo caminho do controller para auth

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const AuthController = require('../Controllers/AuthController');
+const AuthController = require('../Controllers/auth');
 
 // Telas (GET)
 router.get('/login', AuthController.renderLogin);
